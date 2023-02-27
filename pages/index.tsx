c4e7016912b28cfc3e3cfdfb74ec7b009ac3d254
Jakob Evangelista <jakobevangelista@tamu.edu>
2023-02-27 13:33:34 -0600

    added new text to see if ci/cd works

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,9 @@ export default function Home() {
           <div className='text-center py-3 px-3 rounded-md mt-64 text-white hover:bg-grey-500  cursor-pointer'>
             Settings
           </div>
+          <div className='text-center py-3 px-3 rounded-md mt-64 text-white hover:bg-grey-500  cursor-pointer'>
+            PLEASE WORK
+          </div>
           <div className=''></div>
         </div>
         <div className='flex flex-col w-5/6 bg-[#202123] h-screen'></div>
